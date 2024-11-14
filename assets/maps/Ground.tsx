b10ee4dd1ff3c0282fd5963ab520fbcf95558c13
Edson Shivuri <eshivuri@sentillia.com>
2024-11-14 23:54:25 +0100

    feat(tileMap): got tilemap to load correctly

---
+++ assets/maps/Ground.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Ground" tilewidth="128" tileheight="128" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 01.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 02.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 03.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 04.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 05.png" width="128" height="128"/>
+ </tile>
+ <tile id="5">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 06.png" width="128" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 07.png" width="128" height="128"/>
+ </tile>
+ <tile id="7">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 08.png" width="128" height="128"/>
+ </tile>
+ <tile id="8">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 09.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 10.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 11.png" width="128" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 12.png" width="128" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="tileset-resources/Ground Tileset/Village Set_Platformer - Ground 13.png" width="128" height="128"/>
+ </tile>
+</tileset>
